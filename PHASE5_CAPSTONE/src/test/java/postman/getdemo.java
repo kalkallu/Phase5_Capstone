399b632d26d788a5de879d2b2815a3ef2cb96c72
charitha Kalluru (kalkallu) <kalkallu@cisco.com>
2023-06-20 17:31:41 +0530

    Initial commit

---
+++ PHASE5_CAPSTONE/src/test/java/postman/getdemo.java
@@ -0,0 +1,20 @@
+package postman;
+
+import org.testng.annotations.Test;
+import io.restassured.RestAssured;
+
+public class getdemo 
+{
+	@Test
+	public void getresourceinfo()
+	{
+		 RestAssured
+ 	  	.when()
+ 	  		.get("https://gorest.co.in/public/v2/users")
+ 	  	.then()	
+ 	  		.assertThat()
+ 	  		.statusCode(200)
+ 	  		.log().all();
+}
+}
+
